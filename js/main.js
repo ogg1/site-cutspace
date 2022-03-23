576c48a692ca3596c3e6e19a03100ec796fdd003
ogg1 <81367529+ogg1@users.noreply.github.com>
2022-03-23 20:01:17 +0200

    add files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.form-input');
+
+input.addEventListener('focus', function() {
+    form.classList.add('form-active');
+});
+
+input.addEventListener('blur', function() {
+    form.classList.remove('form-active');
+});
